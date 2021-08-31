d43954f84ec9140bb1ce472a9d0b02ae7818a9ed
gitproman0211 <wproman0211@outlook.com>
2021-08-31 13:05:26 -0400

    chore: Update spinner

--- packages/crosswise-uikit/src/components/Spinner/Spinner.tsx
+++ packages/crosswise-uikit/src/components/Spinner/Spinner.tsx
@@ -1,52 +1,15 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
-import PanIcon from "./PanIcon";
-import CrosswiseIcon from "./CrosswiseIcon";
+import styled from "styled-components";
 import { SpinnerProps } from "./types";
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const float = keyframes`
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(10px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
-`;
 
 const Container = styled.div`
   position: relative;
 `;
 
-const RotatingCrosswiseIcon = styled(CrosswiseIcon)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  animation: ${rotate} 2s linear infinite;
-  transform: translate3d(0, 0, 0);
-`;
-
-const FloatingPanIcon = styled(PanIcon)`
-  animation: ${float} 6s ease-in-out infinite;
-  transform: translate3d(0, 0, 0);
-`;
-
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
-      <RotatingCrosswiseIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      <img src={require("./spinner.gif")} width={`${size}px`} />
     </Container>
   );
 };
